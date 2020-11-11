62ac5efd3abb2efdc9d136ed8d9a3ed9016b06da
Mathius Johnson <devachievem@gmail.com>
2020-11-10 20:47:26 -0800

    login and register implemented

--- express_server.js
+++ express_server.js
@@ -2,14 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 const { checkForUserByEmail, checkForEmailInDatabase, generateRandomString, urlsForUserID, timeStamp } = require('./helpers');
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
 };
 
 app.get("/", (req, res) => {
@@ -24,14 +39,15 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.set("view engine", "ejs");
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const user = users[req.cookies['user_id']];
+
+  let templateVars = {
+    urls: urlDatabase,
+    user
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -45,9 +61,12 @@ app.post("/urls", (req, res) => {
 
 // /:shorturl
 app.get("/urls/:shortURL", (req, res) => {
-  console.log("req: ", req);
-  const templateVars = { 
-    shortURL: req.params.shortURL, longURL: req.params.url };
+  const user = users[req.cookies['user_id']];
+
+  let templateVars = { 
+    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],
+    user  
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -55,8 +74,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect(302, '/urls');
 })
+
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const user = users[req.cookies['user_id']];
+
+  let templateVars = {
+    user,
+    urls: urlDatabase
+  };
+  console.log(templateVars);
+
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/:shortURL/update", (req, res) => {
@@ -72,4 +100,71 @@ app.get("/u/:shortURL", (req, res) => {
   console.log("shorturl: ", req.params.shortURL);
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.get("/login", (req, res) => {
+  const user = users[req.cookies['user_id']];
+  let templateVars = {
+    user
+  };
+  res.render('login' ,templateVars);
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const validUser = checkValidUser(email, password);
+  console.log(email, password, validUser);
+  if (validUser) {
+    res.cookie('user_id', validUser.id);
+    res.redirect('/urls');
+  } else {
+    res.status(403).send('<h1> Error 403. Forbidden. Invalid Credentials.');
+  }
+});
+
+app.get("/logout", (req, res) => {
+  const user = users[req.cookies['user_id']];
+  let templateVars = {
+    user
+  };
+  res.render('logout' ,templateVars);
+});
+
+app.post("/logout", (req, res) => {
+  const userEmail = req.body.email;
+  res.clearCookie('user_id');  
+  res.redirect('/urls');
+});
+
+app.get("/register", (req, res) => {
+  const user = users[req.cookies['user_id']];
+  let templateVars = {
+    user
+  };
+  res.render('register' ,templateVars);
+});
+
+
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (password === null) {
+    res.status(400).send('<h1>Status of 400: Bad Request. Cannot leave Email or Password blank');
+  }
+
+  const user = checkForUserByEmail(email);
+
+  if (!user) {
+    const userId = newUser(email, password);
+
+    res.cookie('user_id', userId);
+    console.log(users);
+    res.redirect('/urls');
+  } else {
+    res.status(400).send('<h1>Status of 400: Bad Request. Email Already In Use</h1>');
+  }
 });
